1c2d5358597a9363d2ccc425cb18b9e1e046197c
Ricky <52875633+RICKYLIKEBIRD@users.noreply.github.com>
2023-08-21 23:40:28 +0800

    update 1.測試規避cors

--- src/main/java/dev/ricky/movie/controller/MovieController.java
+++ src/main/java/dev/ricky/movie/controller/MovieController.java
@@ -5,15 +5,13 @@ import dev.ricky.movie.service.MovieService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/movie")
+@CrossOrigin(origins = "http://localhost:3000")
 public class MovieController {
 
     @Autowired
