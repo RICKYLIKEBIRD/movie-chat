2b5af392dd0aaf07eb4a18d4eb571cc4592354e0
Ricky <52875633+RICKYLIKEBIRD@users.noreply.github.com>
2023-08-21 23:21:50 +0800

    update 1.測試規避cors

--- src/main/java/dev/ricky/movie/controller/ReviewController.java
+++ src/main/java/dev/ricky/movie/controller/ReviewController.java
@@ -6,13 +6,11 @@ import dev.ricky.movie.service.ReviewService;
 import dev.ricky.movie.service.dto.CreateReviewDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/review")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ReviewController {
 
     @Autowired
